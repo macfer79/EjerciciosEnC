d817121b7734231541b79036780b111a5a07f543
macfer79 <149889281+macfer79@users.noreply.github.com>
2026-03-14 23:32:02 +0100

    se añade forzado de codificación UTF-8 para Windows y se elimina residuos en el buffer de entrada

--- Ejercicio001/Ejercicio001/Ejercicio001.cpp
+++ Ejercicio001/Ejercicio001/Ejercicio001.cpp
@@ -1,11 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS		//elimina las advertencias propias de Microsoft por utilizar funciones estandar que pueden causar (buffer overflows).
 
 //Librerías
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include <stdio.h>					//librería estandar para que el programa se comunique con el usuario.
 #include <stdlib.h>					//libreria estandar que contiene funciones para control de memoria, procesos del sistema y conversiones de datos.
+#include <locale.h>
 
 int main()
 {
+	setlocale(LC_ALL, "");
+
+    // Solo para Windows: Forzar la consola a usar UTF-8 (65001)
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    #endif
+
 	//Declaración de variables
 	int n = 0;						//contador de cantidad de productos en la lista.
 	int producto;					//número de producto.
@@ -16,7 +28,7 @@ int main()
 	do {
 		//Introducción del código de producto por el usuario.
 		do {
-			printf("Introduce el c%cdigo del producto: ", 162);
+			printf("Introduce el código del producto: ");
 			scanf("%d", &producto);
 		} while (producto < 0);
 
@@ -47,6 +59,12 @@ int main()
 	printf("El programa ha terminado.\n");
 
 	printf("Presiona Enter para salir...");
+
+	// Limpia el búfer: lee caracteres hasta encontrar un \n o el fin del archivo
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+
+    // Ahora sí, este se detendrá de verdad
 	getchar();						//Espera a que el usuario presione una tecla (Enter)
 
 	return 0;
